9e6ccecc73d56409f373c3d7e9552f68a6de3145
Mike Lau <>
2025-02-21 04:36:57 +0800

    Bumped version to 0.0.3

--- loungemembership/build.gradle.kts
+++ loungemembership/build.gradle.kts
@@ -75,7 +75,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.mikekpl"
                 artifactId = "loungemembership"
-                version = "0.0.2"
+                version = "0.0.3"
             }
         }
     }
